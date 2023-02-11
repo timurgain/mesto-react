77a1c6f3b72c2ecaea0eb0ed89213e9d45ecbce8
Timur Gain <timur.gain@gmail.com>
2023-02-11 11:39:12 +0300

    feat: year calc in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__paragraph">© 2022 Mesto Russia</p>
+      <p className="footer__paragraph">© {new Date().getFullYear()} Mesto Russia</p>
     </footer>
   )
 }
